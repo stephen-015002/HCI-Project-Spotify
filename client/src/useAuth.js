59fed43220bebe3f7cb2cbb1945a7b33ce9b8271
Stephen <siskandar@student.unimelb.edu.au>
2023-04-06 04:20:39 +1000

    refresh token added

--- client/src/useAuth.js
+++ client/src/useAuth.js
@@ -19,5 +19,18 @@ export default function useAuth(code) {
         })
     }, [code])
 
+    useEffect(() => {
+        axios.post('http://localhost:3001/refresh', {
+            refreshToken, 
+        }).then(res => {
+            // setAccessToken(res.data.accessToken)
+            // setRefreshToken(res.data.refreshToken)
+            // setExpiresIn(res.data.expiresIn)
+            // window.history.pushState({}, null, "/")
+        }).catch(() => {
+            window.location = '/'
+        })
+    }, [refreshToken, expiresIn])
+
     return accessToken
 }
--- server/server.js
+++ server/server.js
@@ -6,6 +6,22 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.post('/refresh', (req, res) => {
+    const refreshToken = req.body.refreshToken
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: 'http://localhost:3000',
+        clientId: '5b211a2d15e54da686a08946810f4947',
+        clientSecret: 'd4e31cbb0dc9474fb48a07f246213833',
+        refreshToken
+    })
+
+    spotifyApi.refreshAccessToken().then((data) => {
+        console.log(data.body)
+    }).catch(() => {
+        res.sendStatus(400)
+    })
+})
+
 app.post('/login', (req,res) => {
     const code = req.body.code
     const spotifyApi = new SpotifyWebApi({
