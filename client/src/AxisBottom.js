f1c395e3e3d8f3495df163703692336e7853756c
Stephen <siskandar@student.unimelb.edu.au>
2023-05-05 09:22:04 +1000

    axis bottom done

--- client/src/AxisBottom.js
+++ client/src/AxisBottom.js
@@ -1,2 +1,51 @@
 import { useMemo } from 'react';
 import { scaleLinear } from 'd3';
+
+const TICK_LENGTH = 10;
+
+export default function AxisLeft({xScale, pixelsPerTick, height}) {
+    const range = xScale.range();
+
+    const ticks = useMemo(() => {
+        const width = range[0] - range[1];
+        const numberOfTicksTarget = Math.floor(width / pixelsPerTick);
+
+        return xScale.ticks(numberOfTicksTarget).map((value) => ({
+            value,
+            xOffset: xScale(value),
+        }))
+    }, [xScale, pixelsPerTick, range]);
+
+    return (
+        <>
+            {/* Ticks and labels */}
+            {ticks.map(({value, xOffset}) => (
+                <g 
+                    key={value}
+                    transform={`translate(0, ${xOffset})`}
+                    shapeRendering={'crispEdges'}
+                >
+                    <line
+                        y1={TICK_LENGTH}
+                        y2={-height - TICK_LENGTH}
+                        stroke='#D2D7D3'
+                        strokeWidth={0.5}
+                    />
+                    <text
+                        key={value}
+                        style={{
+                            fontSize: '10px',
+                            textAnchor: 'middle',
+                            transform: 'translateX(20px)',
+                            fill: '#D2D7D3'
+                        }}
+                    >
+                        {value}
+                    </text>
+
+                </g>
+            ))}
+        </>
+    )
+
+}
